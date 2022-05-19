41a8b38a10a555d0d0f16d0beb4621c9036d2b9c
AngelaEspinosa <titagena@gmail.com>
2022-05-19 17:41:13 -0500

    AGREGADO USUARIO: se completan las validaciones al  objeto de valor NombreCompleto.

--- domain/src/main/java/co/com/clinica_veterinaria/atencion_al_usuario/usuario/Dueño.java
+++ domain/src/main/java/co/com/clinica_veterinaria/atencion_al_usuario/usuario/Dueño.java
@@ -1,9 +1,16 @@
 package co.com.clinica_veterinaria.atencion_al_usuario.usuario;
 
+import co.com.clinica_veterinaria.atencion_al_usuario.usuario.values.DatosDeContacto;
 import co.com.clinica_veterinaria.atencion_al_usuario.usuario.values.DueñoId;
+import co.com.clinica_veterinaria.atencion_al_usuario.usuario.values.NombreCompleto;
 import co.com.sofka.domain.generic.Entity;
 
 public class Dueño extends Entity<DueñoId> {
+
+    private NombreCompleto nombreCompleto;
+    private DatosDeContacto datosDeContacto;
+
+
     public Dueño(DueñoId dueñoId) {
         super(dueñoId);
     }
--- domain/src/main/java/co/com/clinica_veterinaria/atencion_al_usuario/usuario/values/NombreCompleto.java
+++ domain/src/main/java/co/com/clinica_veterinaria/atencion_al_usuario/usuario/values/NombreCompleto.java
@@ -1,2 +1,37 @@
-package co.com.clinica_veterinaria.atencion_al_usuario.usuario;public class NombreCompleto {
+package co.com.clinica_veterinaria.atencion_al_usuario.usuario.values;
+
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class NombreCompleto implements ValueObject<String> {
+    private final String value;
+
+    public NombreCompleto(String value) {
+        this.value = Objects.requireNonNull(value);
+        if(this.value.isBlank()){
+            new IllegalArgumentException("El Nombre no puede estar vacio.");
+        }
+        if(this.value.length() <= 2){
+            new IllegalArgumentException("Debe de tener mayor a 4 caracteres.");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof NombreCompleto)) return false;
+        NombreCompleto that = (NombreCompleto) o;
+        return Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String value() {
+        return null;
+    }
 }
