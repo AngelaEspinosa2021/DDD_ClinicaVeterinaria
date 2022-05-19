f7b28016bc217a35e7caab7effc62b7a3583d4a9
AngelaEspinosa <titagena@gmail.com>
2022-05-19 17:51:59 -0500

    AGREGADO USUARIO: Se inicializan los atributos de la entidad DUEÑO dentro del constructor correspondiente.

--- domain/src/main/java/co/com/clinica_veterinaria/atencion_al_usuario/usuario/Dueño.java
+++ domain/src/main/java/co/com/clinica_veterinaria/atencion_al_usuario/usuario/Dueño.java
@@ -1,17 +1,34 @@
 package co.com.clinica_veterinaria.atencion_al_usuario.usuario;
 
-import co.com.clinica_veterinaria.atencion_al_usuario.usuario.values.DatosDeContacto;
+import co.com.clinica_veterinaria.atencion_al_usuario.values_generic.DatosDeContacto;
 import co.com.clinica_veterinaria.atencion_al_usuario.usuario.values.DueñoId;
-import co.com.clinica_veterinaria.atencion_al_usuario.usuario.values.NombreCompleto;
+import co.com.clinica_veterinaria.atencion_al_usuario.values_generic.FechaDeNacimiento;
+import co.com.clinica_veterinaria.atencion_al_usuario.values_generic.NombreCompleto;
 import co.com.sofka.domain.generic.Entity;
 
 public class Dueño extends Entity<DueñoId> {
 
     private NombreCompleto nombreCompleto;
     private DatosDeContacto datosDeContacto;
+    private FechaDeNacimiento fechaDeNacimiento;
 
 
-    public Dueño(DueñoId dueñoId) {
+    public Dueño(DueñoId dueñoId,NombreCompleto nombreCompleto,DatosDeContacto datosDeContacto,FechaDeNacimiento fechaDeNacimiento){
         super(dueñoId);
+        this.nombreCompleto=nombreCompleto;
+        this.datosDeContacto=datosDeContacto;
+        this.fechaDeNacimiento=fechaDeNacimiento;
+    }
+
+    public NombreCompleto nombreCompleto() {
+        return nombreCompleto;
+    }
+
+    public DatosDeContacto datosDeContacto() {
+        return datosDeContacto;
+    }
+
+    public FechaDeNacimiento fechaDeNacimiento() {
+        return fechaDeNacimiento;
     }
 }
