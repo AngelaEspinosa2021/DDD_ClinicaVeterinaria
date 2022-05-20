098bbff91c3f46b6a73c595abc0a4440dd00871a
AngelaEspinosa <titagena@gmail.com>
2022-05-19 21:02:03 -0500

    AGREGADO USUARIO: Se hacen correcciones sobre las entidades HistoriaMedica y Paciente. Se crean los metodos de cada entidad con sus objetos de valor completamente validados.

---
+++ domain/src/main/java/co/com/clinica_veterinaria/atencion_al_usuario/usuario/values/Observacion.java
@@ -0,0 +1,18 @@
+package co.com.clinica_veterinaria.atencion_al_usuario.usuario;
+
+import co.com.sofka.domain.generic.ValueObject;
+
+import java.util.Objects;
+
+public class Observacion implements ValueObject<String> {
+    private final String observacion;
+
+    public Observacion(String observacion) {
+        this.observacion = Objects.requireNonNull(observacion);
+    }
+
+    @Override
+    public String value() {
+        return observacion;
+    }
+}
