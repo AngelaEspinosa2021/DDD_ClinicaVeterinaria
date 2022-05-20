208b7e4cd4ee182ed3bca96970a3582679f3c144
AngelaEspinosa <titagena@gmail.com>
2022-05-20 13:27:22 -0500

    BUSINESS: Se logra la creacion del caso de uso CrearUsuarioUseCase del agregado USUARIO.

--- business/src/main/java/co/com/clinica_veterinaria/atencion_al_usuario/usuario/CrearUsuarioUseCase.java
+++ business/src/main/java/co/com/clinica_veterinaria/atencion_al_usuario/usuario/CrearUsuarioUseCase.java
@@ -1,8 +1,16 @@
 package co.com.clinica_veterinaria.atencion_al_usuario.usuario;
 
+import co.com.clinica_veterinaria.atencion_al_usuario.usuario.commands.CrearUsuario;
 import co.com.sofka.business.generic.UseCase;
 import co.com.sofka.business.support.RequestCommand;
 import co.com.sofka.business.support.ResponseEvents;
 
-public class CrearUsuarioUseCase extends UseCase<RequestCommand<>, ResponseEvents> {
+public class CrearUsuarioUseCase extends UseCase<RequestCommand<CrearUsuario>, ResponseEvents> {
+    @Override
+    public void executeUseCase(RequestCommand<CrearUsuario> crearUsuarioRequestCommand) {
+        var command = crearUsuarioRequestCommand.getCommand();
+        var usuario = new Usuario(command.getUsuarioId(), command.getFechaDeCreacion());
+
+        emit().onResponse(new ResponseEvents(usuario.getUncommittedChanges()));
+    }
 }
--- domain/src/main/java/co/com/clinica_veterinaria/atencion_al_usuario/usuario/commands/CrearUsuario.java
+++ domain/src/main/java/co/com/clinica_veterinaria/atencion_al_usuario/usuario/commands/CrearUsuario.java
@@ -1,17 +1,23 @@
 package co.com.clinica_veterinaria.atencion_al_usuario.usuario.commands;
 
+import co.com.clinica_veterinaria.atencion_al_usuario.usuario.values.UsuarioId;
 import co.com.clinica_veterinaria.atencion_al_usuario.values_generic.Fecha;
 import co.com.sofka.domain.generic.Command;
 
 public class CrearUsuario extends Command {
-
+    private final UsuarioId usuarioId;
     private final Fecha fechaDeCreacion;
 
-    public CrearUsuario(Fecha fechaDeCreacion) {
+    public CrearUsuario(UsuarioId usuarioId,Fecha fechaDeCreacion) {
+        this.usuarioId = usuarioId;
         this.fechaDeCreacion = fechaDeCreacion;
     }
 
     public Fecha getFechaDeCreacion() {
         return fechaDeCreacion;
+    }
+
+    public UsuarioId getUsuarioId() {
+        return usuarioId;
     }
 }
