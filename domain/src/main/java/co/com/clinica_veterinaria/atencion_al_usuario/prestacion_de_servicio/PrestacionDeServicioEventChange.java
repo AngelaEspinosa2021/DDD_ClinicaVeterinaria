35af8419a99e6665067c1caccb6c117a6d539a2a
AngelaEspinosa <titagena@gmail.com>
2022-05-20 02:18:31 -0500

    AGREGADO PRESTACION DE SERVICIOS: Se crea el PrestacionDeServicioEventChange donde se llaman cada uno de los eventos y se le incluyen las afectaciones a los estados que le corresponde a cada uno.

---
+++ domain/src/main/java/co/com/clinica_veterinaria/atencion_al_usuario/prestacion_de_servicio/PrestacionDeServicioEventChange.java
@@ -0,0 +1,8 @@
+package co.com.clinica_veterinaria.atencion_al_usuario.prestacion_de_servicio;
+
+import co.com.sofka.domain.generic.EventChange;
+
+public class PrestacionDeServicioEventChange extends EventChange {
+    public PrestacionDeServicioEventChange(PrestacionDeServicio prestacionDeServicio) {
+    }
+}
