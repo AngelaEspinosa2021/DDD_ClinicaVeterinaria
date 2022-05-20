ce1840c8108ad7390fbbbf91f04f993a3e86adae
AngelaEspinosa <titagena@gmail.com>
2022-05-20 03:47:22 -0500

    AGREGADO ATENCION MEDICA: Se logra generar cada uno de los comportamientos del agregado, con la asociacion a cada uno de los eventos. // PENDIENTE DE LA IMPLEMENTACION DE CADA EVENTO.

--- domain/src/main/java/co/com/clinica_veterinaria/atencion_al_usuario/atencion_medica/AtencionMedica.java
+++ domain/src/main/java/co/com/clinica_veterinaria/atencion_al_usuario/atencion_medica/AtencionMedica.java
@@ -1,16 +1,20 @@
 package co.com.clinica_veterinaria.atencion_al_usuario.atencion_medica;
 
 import co.com.clinica_veterinaria.atencion_al_usuario.atencion_medica.events.AtencionCreada;
-import co.com.clinica_veterinaria.atencion_al_usuario.atencion_medica.values.AtencionId;
-import co.com.clinica_veterinaria.atencion_al_usuario.values_generic.Estado;
-import co.com.clinica_veterinaria.atencion_al_usuario.atencion_medica.values.TipoDeAtencion;
+import co.com.clinica_veterinaria.atencion_al_usuario.atencion_medica.values.*;
+import co.com.clinica_veterinaria.atencion_al_usuario.prestacion_de_servicio.events.PrestacionDeServicioCreado;
+import co.com.clinica_veterinaria.atencion_al_usuario.prestacion_de_servicio.values.Prioridad;
+import co.com.clinica_veterinaria.atencion_al_usuario.usuario.values.Descripcion;
+import co.com.clinica_veterinaria.atencion_al_usuario.usuario.values.HistoriaMedicaId;
+import co.com.clinica_veterinaria.atencion_al_usuario.values_generic.*;
 import co.com.clinica_veterinaria.atencion_al_usuario.prestacion_de_servicio.PrestacionDeServicio;
 import co.com.clinica_veterinaria.atencion_al_usuario.prestacion_de_servicio.values.ServicioId;
 import co.com.clinica_veterinaria.atencion_al_usuario.usuario.values.UsuarioId;
-import co.com.clinica_veterinaria.atencion_al_usuario.values_generic.Fecha;
 import co.com.sofka.domain.generic.AggregateEvent;
 
 import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
 
 public class AtencionMedica extends AggregateEvent<AtencionId> {
     protected Fecha fechaDeAtencion;
@@ -19,7 +23,7 @@ public class AtencionMedica extends AggregateEvent<AtencionId> {
     protected UsuarioId usuarioId;
     protected Medico medico;
     protected Urgencia urgencia;
-    protected Map<ServicioId, PrestacionDeServicio> servicios;
+    protected Set<PrestacionDeServicio> servicios;
     protected CitaProgramada citaProgramada;
 
 
@@ -27,4 +31,54 @@ public class AtencionMedica extends AggregateEvent<AtencionId> {
         super(atencionId);
         appendChange( new AtencionCreada(fechaDeAtencion,tipoDeAtencion,estado)).apply();
     }
+
+    public void agregarMedico(MedicoId medicoId, NombreCompleto nombreCompleto, DatosDeContacto datosDeContacto, Fecha fechaDeNacimiento){
+        appendChange( new MedicoAgregado(medicoId,nombreCompleto,datosDeContacto,fechaDeNacimiento)).apply();
+    }
+
+    public void solicitarAtencionDeUrgencia(UrgenciaId urgenciaId, Observacion observacion, Triage triage){
+        appendChange(new AtencionDeUrgenciaSolicitada(urgenciaId,observacion,triage)).apply();
+    }
+
+    public void solicitarAtencionDeCitaProgramada(CitaId CitaId, Observacion observacion,FechaCita fechaCita){
+        appendChange(new AtencionDeCitaProgramadaSolicitada(CitaId,observacion,fechaCita)).apply();
+    }
+
+    public void actualizarDatosDeContactoDeMedico(MedicoId medicoId, DatosDeContacto datosDeContacto){
+        appendChange(new DatosDeContactoDeMedicoActualizados(medicoId,datosDeContacto)).apply();
+    }
+
+    public void actualizarNombreDeMedico(MedicoId medicoId, NombreCompleto nombreCompleto){
+        appendChange(new NombreDeMedicoActualizado(medicoId,nombreCompleto)).apply();
+    }
+
+    public void actualizarFechaDeCitaProgramada(CitaId citaId, FechaCita fechaCita){
+        appendChange( new FechaDeCitaProgramadaActualizada(citaId,fechaCita)).apply();
+    }
+
+    public void actualizarObservacionesDeCitaProgramada(CitaId citaId, Observacion observacion){
+        appendChange( new ObservacionesDeCitaProgramadaActualizada(citaId,observacion)).apply();
+    }
+
+    public void actualizarObservacionDeUrgencia(UrgenciaId urgenciaId, Observacion observacion){
+        appendChange( new ObservacionDeUrgenciaActualizada(urgenciaId,observacion)).apply();
+    }
+
+    public void agregarAHistoriaMedicaDeUsuario(UsuarioId usuarioId,HistoriaMedicaId historiaMedicaId, Fecha fecha, Descripcion descripcion ){
+        appendChange(new HistoriaMedicaDeUsuarioAgregada(usuarioId,historiaMedicaId,fecha,descripcion)).apply();
+    }
+
+    public void solicitarPrestacionDeServicio(ServicioId servicioId, Prioridad prioridad, Fecha fechaDeSolicitud){
+        appendChange(new PrestacionDeServicioSolicitado(servicioId,prioridad,fechaDeSolicitud)).apply();
+    }
+
+    public void solicitarAtencionDeUrgencias(ServicioId servicioId,UrgenciaId urgenciaId,Observacion observacion,Triage triage){
+        appendChange(new AtencionDeUrgenciasSolicitada(servicioId,urgenciaId,observacion,triage)).apply();
+    }
+
+    public void solicitarAtencionDeCitaProgramada(ServicioId servicioId,CitaId CitaId, Observacion observacion,FechaCita fechaCita){
+        appendChange( new AtencionDeCitaProgramadaSolicitada(servicioId,CitaId,observacion,fechaCita)).apply();
+    }
+
+
 }
