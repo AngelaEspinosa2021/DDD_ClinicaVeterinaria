1243592970fb13f19b884ce741f4f89c222a40cd
AngelaEspinosa <titagena@gmail.com>
2022-05-20 02:29:46 -0500

    AGREGADO PRESTACION DE SERVICIOS: Se logran crear cada uno de los comandos asociados a los comportamientos del agregado.

---
+++ domain/src/main/java/co/com/clinica_veterinaria/atencion_al_usuario/prestacion_de_servicio/commands/SolicitarHospitalizacion.java
@@ -0,0 +1,2 @@
+package co.com.clinica_veterinaria.atencion_al_usuario.prestacion_de_servicio.commands;public class SolicitarHospitalizacion {
+}
---
+++ domain/src/main/java/co/com/clinica_veterinaria/atencion_al_usuario/prestacion_de_servicio/commands/SolicitarPrestacionDeServicio.java
@@ -0,0 +1,4 @@
+package co.com.clinica_veterinaria.atencion_al_usuario.prestacion_de_servicio.commands;
+
+public class SolicitarPrestacionDeServicio {
+}
