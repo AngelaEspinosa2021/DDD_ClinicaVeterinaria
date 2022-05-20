fc4c3e18f3f2385e801979f213873e2b614ffd33
AngelaEspinosa <titagena@gmail.com>
2022-05-20 16:58:40 -0500

    AGREGADO USUARIO / TESTS: se crea el CrearHistoriaMedicaUseCase para poder evaluar el funcionamiento del comando asociado a esa entidad.

--- business/src/main/java/co/com/clinica_veterinaria/atencion_al_usuario/usuario/CrearHistoriaMedicaUseCase.java
+++ business/src/main/java/co/com/clinica_veterinaria/atencion_al_usuario/usuario/CrearHistoriaMedicaUseCase.java
@@ -1,2 +1,16 @@
-package co.com.clinica_veterinaria.atencion_al_usuario.usuario;public class CrearHistoriaMedica {
+package co.com.clinica_veterinaria.atencion_al_usuario.usuario;
+
+import co.com.clinica_veterinaria.atencion_al_usuario.usuario.commands.CrearHistoriaMedica;
+import co.com.sofka.business.generic.UseCase;
+import co.com.sofka.business.support.RequestCommand;
+import co.com.sofka.business.support.ResponseEvents;
+
+public class CrearHistoriaMedicaUseCase extends UseCase<RequestCommand<CrearHistoriaMedica>, ResponseEvents> {
+    @Override
+    public void executeUseCase(RequestCommand<CrearHistoriaMedica> crearHistoriaMedicaRequestCommand) {
+        var command = crearHistoriaMedicaRequestCommand.getCommand();
+        var usuario = Usuario.from(command.getUsuarioId(), repository().getEventsBy(command.getUsuarioId().value()));
+        usuario.crearHistoriaMedica(command.getHistoriaMedicaId(), command.getFecha(),command.getDescripcion());
+        emit().onResponse(new ResponseEvents(usuario.getUncommittedChanges()));
+    }
 }
--- domain/src/main/java/co/com/clinica_veterinaria/atencion_al_usuario/usuario/commands/CrearHistoriaMedica.java
+++ domain/src/main/java/co/com/clinica_veterinaria/atencion_al_usuario/usuario/commands/CrearHistoriaMedica.java
@@ -2,18 +2,25 @@ package co.com.clinica_veterinaria.atencion_al_usuario.usuario.commands;
 
 import co.com.clinica_veterinaria.atencion_al_usuario.usuario.values.Descripcion;
 import co.com.clinica_veterinaria.atencion_al_usuario.usuario.values.HistoriaMedicaId;
+import co.com.clinica_veterinaria.atencion_al_usuario.usuario.values.UsuarioId;
 import co.com.clinica_veterinaria.atencion_al_usuario.values_generic.Fecha;
 import co.com.sofka.domain.generic.Command;
 
 public class CrearHistoriaMedica extends Command {
+    private final UsuarioId usuarioId;
     private final HistoriaMedicaId historiaMedicaId;
     private final Fecha fecha;
     private final Descripcion descripcion;
 
-    public CrearHistoriaMedica(HistoriaMedicaId historiaMedicaId, Fecha fecha, Descripcion descripcion) {
+    public CrearHistoriaMedica(UsuarioId usuarioId, HistoriaMedicaId historiaMedicaId, Fecha fecha, Descripcion descripcion) {
+        this.usuarioId = usuarioId;
         this.historiaMedicaId = historiaMedicaId;
         this.fecha = fecha;
         this.descripcion = descripcion;
+    }
+
+    public UsuarioId getUsuarioId() {
+        return usuarioId;
     }
 
     public HistoriaMedicaId getHistoriaMedicaId() {
